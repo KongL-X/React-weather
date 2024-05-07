a056bdcebe79f828cecc6c7e43d482a056297442
lixue <3069673268@qq.com>
2024-05-07 17:33:06 +0800

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '64b096485cmsh8b5cdd368a14bc0p131b2cjsn5b8daddbd498',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL='https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = "a9e630b58022603ea2850628da6bdad4";
